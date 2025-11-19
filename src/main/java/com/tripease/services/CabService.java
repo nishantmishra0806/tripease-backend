03389076e6298c568310a9ab1e9d20038a445cd9
nishantmishra0806 <nishantmishra0806@gmail.com>
2025-11-20 01:04:31 +0530

    Added kafka Producer and Consumer for location broadcasting

--- src/main/java/com/tripease/services/CabService.java
+++ src/main/java/com/tripease/services/CabService.java
@@ -1,15 +1,21 @@
 package com.tripease.services;
 
 import com.tripease.dto.request.CabRequest;
+import com.tripease.dto.response.CabDetailsResponse;
 import com.tripease.dto.response.CabResponse;
+import com.tripease.entities.Booking;
 import com.tripease.entities.Cab;
+import com.tripease.entities.Customer;
 import com.tripease.entities.Driver;
 import com.tripease.exceptions.DriverNotFoundException;
 import com.tripease.repository.CabRepo;
 import com.tripease.repository.DriverRepo;
 import com.tripease.transformers.CabTransformer;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,8 +23,10 @@ public class CabService {
 
 
     private DriverRepo driverRepo;
-    public CabService(  DriverRepo driverRepo) {
+    private CabRepo cabRepo;
+    public CabService(  DriverRepo driverRepo , CabRepo cabRepo) {
         this.driverRepo = driverRepo;
+        this.cabRepo = cabRepo;
     }
 
     public CabResponse registerCab(CabRequest  cabRequest , int driverId) {
@@ -34,5 +42,34 @@ public class CabService {
         //we are saving driver and we have use CASCADEALL which will automatically saved into child entity
         Driver savedDriver = driverRepo.save(driver);
         return CabTransformer.cabToCabResponse(savedDriver.getCab(), savedDriver) ;
+    }
+
+    public CabDetailsResponse getCabDetails(@RequestParam int cabId) {
+        Optional<Driver> optionalDriver = driverRepo.findById(cabId);
+        if(optionalDriver.isEmpty()){
+            throw new DriverNotFoundException("Driver not found");
+        }
+        Driver driver = optionalDriver.get();
+        Optional<Cab> optionalCab = cabRepo.findById(cabId);
+        if(optionalCab.isEmpty()){
+            throw new RuntimeException("Cab not found");
+        }
+        Cab cab = optionalCab.get();
+
+        List<Booking> book = driver.getBookings();
+        List<Customer> cust = new ArrayList<>();
+        for(Booking booking : book){
+            cust.add(booking.getCustomer());
+        }
+
+        CabDetailsResponse response = CabDetailsResponse.builder()
+                .cabId(cabId)
+                .cabModel(cab.getCabModel())
+                .cabNumber(cab.getCabNumber())
+                .driver(driver)
+                .customer(cust).build();
+
+
+        return response;
     }
 }
